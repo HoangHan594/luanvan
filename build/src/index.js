3efec165bd44f8160045cee9131e244372d56e1d
HoangHan594 <nhathoang19012003@gmail.com>
2024-11-01 22:11:32 +0700

    rebuild api

--- build/src/index.js
+++ build/src/index.js
@@ -6,41 +6,38 @@ const moment = require("moment");
 const cookieParser = require('cookie-parser');
 const path = require("path");
 const cors = require('cors');
-const cronJobs = require('./cron/cronJobs.js');
-
+const cronJobs = require('../../cron/cronJobs.js');
 require("dotenv").config();
-
-const database = require('./config/database');
+const database = require('../../config/database');
 database.connect();
-
-const clientRoute = require("./routes/client/index.route");
-const adminRoute = require("./routes/admin/index.route");
-const systemPrefix = require("./config/system")
+const clientRoute = require("../../routes/client/index.route");
+const adminRoute = require("../../routes/admin/index.route");
+const systemPrefix = require("../../config/system");
 const mongoose = require('mongoose');
-
-
 const app = express();
 const port = process.env.PORT;
 
 // cookies
 app.use(cookieParser('KWJFKWEIFHW'));
-app.use(session({ cookie: { maxAge: 60000 } }));
-
-app.use(express.static('public'))
-    // override
+app.use(session({
+    cookie: {
+        maxAge: 60000
+    }
+}));
+app.use(express.static('public'));
+// override
 app.use(methodOverride('_method'));
 const corsOptions = {
     origin: ['https://luanvan-fe-3nzp.vercel.app', 'https://luanvan-fe-3nzp-git-master-hoang-hans-projects.vercel.app', 'https://luanvan-fe-3nzp-jlpirbmfq-hoang-hans-projects.vercel.app'],
-    credentials: true, // nếu bạn sử dụng cookie
+    credentials: true // nếu bạn sử dụng cookie
 };
-
 app.use(cors(corsOptions));
 
-
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.json());
-
 
 // Flash
 // app.use(cookieParser('KWJFKWEIFHW'));
@@ -49,27 +46,24 @@ app.use(express.json());
 // End Flash
 
 // TinyMCE
-app.use(
-    '/tinymce',
-    express.static(path.join(__dirname, "node_modules", "tinymce"))
-);
+app.use('/tinymce', express.static(path.join(__dirname, "node_modules", "tinymce")));
 // End TinyMCE
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
-    });
-
-
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+});
 app.get('/', async(req, res) => {
     const dbState = mongoose.connection.readyState;
     const status = dbState === 1 ? 'connected' : 'disconnected';
-    res.json({ database: status });
+    res.json({
+        database: status
+    });
 });
-
 
 // Variables
 app.locals.adminPrefix = systemPrefix.adminPrefix;
@@ -78,7 +72,6 @@ app.locals.moment = moment;
 // Routes
 clientRoute(app);
 adminRoute(app);
-
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-})
+});
